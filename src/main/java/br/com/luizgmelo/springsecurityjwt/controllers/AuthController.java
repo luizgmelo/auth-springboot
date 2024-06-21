9aeebe6a9277d46ca394e93cef40ea15039faada
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-06-21 12:16:22 -0300

    feat: implement authenticationService to auth a user

--- src/main/java/br/com/luizgmelo/springsecurityjwt/controllers/AuthController.java
+++ src/main/java/br/com/luizgmelo/springsecurityjwt/controllers/AuthController.java
@@ -1,16 +1,22 @@
 package br.com.luizgmelo.springsecurityjwt.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.luizgmelo.springsecurityjwt.security.AuthenticationService;
+
 @RestController
 @RequestMapping("authenticate")
 public class AuthController {
+  @Autowired
+  private AuthenticationService authenticationService;
 
   @PostMapping
-  public String authenticate() {
-    return "token";
+  public String authenticate(Authentication authentication) {
+    return authenticationService.authenticate(authentication);
   }
 
 }

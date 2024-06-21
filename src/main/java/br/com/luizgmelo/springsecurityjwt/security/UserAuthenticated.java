c8be1d28599963dc5ca7d992002735624ea80d46
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-06-21 11:51:02 -0300

    feat: create UserDetails for verify status of the user

---
+++ src/main/java/br/com/luizgmelo/springsecurityjwt/security/UserAuthenticated.java
@@ -0,0 +1,33 @@
+package br.com.luizgmelo.springsecurityjwt.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import br.com.luizgmelo.springsecurityjwt.models.User;
+
+public class UserAuthenticated implements UserDetails {
+  private final User user;
+
+  public UserAuthenticated(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(() -> "read");
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getUsername();
+  }
+
+}
